9db038a945d24fec840459943cdb3229efa2e6e6
Alfredo <almalfredo19@gmail.com>
2019-11-09 11:17:12 -0500

    algunos cambios

--- View/crudobj/ListaobjetosUR.php
+++ View/crudobj/ListaobjetosUR.php
@@ -7,7 +7,7 @@ header("Location:../../index.php");
 }
  require("../../Controllers/Controladores/ObjetoControlador.php");
  $filasObjetos=ObjetoControlador::getObjetos();
-
+ 
  //echo($filasObjetos["nombre"]);
 ?>
 
@@ -16,10 +16,34 @@ header("Location:../../index.php");
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <link rel="stylesheet" href="../../css/style.css">
+
+
 </head>
 <body>
-    <h1>Lista de objetos perdidos</h1>
-       <table id="tabla" class="table table-dark text-light">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a href="../HomeUsuarioAdmin.php" class="nav-link">Inicio</a>
+            </li>
+        </ul>
+        <?php
+
+            echo '
+                <ul class="nav navbar-nav ml-auto">
+                    <li>
+                        <a href="../../Controllers/Accions/AccionVerPerfil.php" class="nav-link active">';echo $_SESSION["usuario"];echo '</a>
+                    </li>
+                    <li>
+                        <a href="../../Controllers/Accions/AccionLogOut.php" class="nav-link">Cerrar sesión</a>
+                    </li>
+                </ul>
+                ';
+            ?>
+   </nav>
+    <h1 class="text-light">Lista de objetos perdidos</h1>
+       <table id="tabla" class="table table-dark text-light table-striped fondolistaobjetos">
                 <thead>
                 <tr>
                 <th>Nombre</th>
@@ -33,12 +57,22 @@ header("Location:../../index.php");
                 <tbody>
                 <?php
                      foreach($filasObjetos as $objetos){
-                         
+                         switch($objetos['estado']){
+                            case 1:
+                                 $estado="perdido";
+                            break;
+                            case 2:
+                                  $estado="tramite";
+                            break;
+                            case 3:
+                                 $estado="entregado";                             
+                            break;
+                         } 
                      ?>
                      <tr>
                         <td><?php echo $objetos['nombre']?></td>
                         <td> <img  src="data:image/jpg;base64,<?php echo base64_encode($objetos['foto']);?>" alt="hola"></td>
-                        <td><?php echo $objetos['estado']?></td> 
+                        <td><?php echo $estado?></td> 
                         <td><?php echo $objetos['contacto']?></td>
                         <td><?php echo $objetos['fecha_reporte']?></td>
                         <td><a href='ver_objeto.php?nombre=<?php echo $objetos['nombre']?>'>Ver más</a></td>
--- View/crudobj/reportar.php
+++ View/crudobj/reportar.php
@@ -1,13 +1,44 @@
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a href="../HomeUsuarioAdmin.php" class="nav-link">Inicio</a>
+            </li>
+        </ul>
+        <?php
+          session_start();
+            echo '
+                <ul class="nav navbar-nav ml-auto">
+                    <li>
+                        <a href="../../Controllers/Accions/AccionVerPerfil.php" class="nav-link active">';echo $_SESSION["usuario"];echo '</a>
+                    </li>
+                    <li>
+                        <a href="../../Controllers/Accions/AccionLogOut.php" class="nav-link">Cerrar sesión</a>
+                    </li>
+                </ul>
+                ';
+            ?>
+   </nav>
+</body>
+</html>
 
 <?php
  require_once("../../Controllers/Controladores/ObjetoControlador.php");
  require(__DIR__."\..\..\Controllers\Controladores\TramiteContralador.php");
-   session_start();
+   
   if($_SESSION["obj"]=="sinreportar"){
        
-        echo "cambbiar estado ".ObjetoControlador::CambiarEstado($_GET["nombreObjeto"],"2");
+        echo "cambiar estado ".ObjetoControlador::CambiarEstado($_GET["nombreObjeto"],"2");
         $fecha= date('Y-m-d H:i:s');
          echo "<br> insertar: ".TramiteControlador::setTramite($_GET["nombreUsuario"],$_GET["nombreObjeto"],$fecha);
         $nombreObjeto=$_GET['nombreObjeto'];
@@ -18,6 +49,7 @@
   }else{// cancelamos el reporte
       echo "cambbiar estado ".ObjetoControlador::CambiarEstado($_GET["nombreObjeto"],"1");
       echo "<br> eliminado: ".TramiteControlador::DeleteTramite($_GET["nombreUsuario"],$_GET["nombreObjeto"]);
+      
   }
   
    
@@ -25,4 +57,4 @@
 
  
 
-?>
+
